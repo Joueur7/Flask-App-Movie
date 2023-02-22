47ac737041920d100887fadf899bd965fa3eaadf
Joueur7 <vsinghinda@gmail.com>
2023-02-22 15:02:19 +0530

    Added .env

--- manager.py
+++ manager.py
@@ -2,9 +2,27 @@ from flask import Flask
 from flask_migrate import Migrate
 from models.movie_model import db
 from controllers.movie_controller import movies_controller
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:strongpass@localhost:3306/moviedb'
+user = os.getenv('DB_USER')
+password = os.getenv('DB_PASS')
+host = os.getenv('DB_HOST')
+port = os.getenv('DB_PORT')
+name = os.getenv('DB_NAME')
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://{}:{}@{}:{}/{}'.format(
+    user,
+    password,
+    host,
+    port,
+    name
+)
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 migrate = Migrate(app, db)
